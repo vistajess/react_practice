8e74e929a9a4e5fc942a4854f47f60e00ab76720
vistajess <vistajesus8@gmail.com>
2015-09-03 19:27:14 +0800

    practice json

---
+++ react-json/component/Customers.jsx
@@ -0,0 +1,13 @@
+var Customers = React.createClass({
+	render : function() {
+		return(
+			<div className="ok">
+			<h2>THIS IS THE CUSTOMER CLASS</h2>
+
+		
+			<Main_component />
+			
+			</div>
+		);
+	}
+});
---
+++ react-json/component/Main_component.jsx
@@ -0,0 +1,11 @@
+var Main_component = React.createClass({
+	render : function() {
+		return(
+			<div className="ok">
+			<h2>Main_component Class</h2>
+
+			
+			</div>
+		);
+	}
+});
